295111750feacba06756d6cb148acdc3fe435548
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-09-07 16:46:19 +0000

    fix: correct inventory selection and implement word length calculation

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -21,13 +21,16 @@ end
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
-  inventory_list.select{|item| 
-  item[:item] < 4}
+  inventory_list.select{|key,value| 
+  value < 4}
 end
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
-  new_list = word_list.reduce{|letter| }
+  new_list = word_list.reduce(Hash.new()){ |length,word|
+    length[word] = word.length
+    length
+  }
 end
